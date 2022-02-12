fded205a7f1ae650d2d159fd921df5fea09d0c0a
manta2021 <99058366+manta2021@users.noreply.github.com>
2022-02-12 09:42:05 +0900

    20220212
    -문자열 예제

---
+++ hongongc/315.cpp
@@ -0,0 +1,23 @@
+//
+//  315.cpp
+//  c++ study
+//
+//  Created by 김혜준 on 2022/02/11.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0, count = 0, z = 0;
+    char ch;
+    
+    ch = getchar();
+    while()
+    {
+        printf("1");
+    }
+    
+    return 0;
+    
+}
