031447e61a156a6611efd043c21306f1135a8111
manta2021 <99058366+manta2021@users.noreply.github.com>
2022-02-19 10:52:25 +0900

    20229219
    -도전실전예제, 확인문제

---
+++ hongongc/352확인문제 2.cpp
@@ -0,0 +1,19 @@
+//
+//  352확인문제 2.cpp
+//  c++ study
+//
+//  Created by 김혜준 on 2022/02/19.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char str[] = "liongg";
+    char *ps = "king";
+    
+    strcpy(str, "cat");
+    
+    return 0;
+}
--- hongongc/353 도전 실전 예제.cpp
+++ hongongc/353 도전 실전 예제.cpp
@@ -8,19 +8,31 @@
 #include <stdio.h>
 #include <string.h>
 
+void swap(char *a,char *b){
+    char temp[80];
+    strcpy(temp,a);
+    strcpy(a, b);
+    strcpy(b, temp);
+}
 int main(void)
 {
     char str1[80], str2[80], str3[80];
-    char *s1, *s2, *s3;
-    
+    char *a = str1,*b = str2,*c = str3;
     printf("세 단어 입력 : ");
     scanf("%s %s %s", str1, str2, str3);
     
-    s1 = str1;
-    s2 = str2;
-    s3 = str3;
+    if(strcmp(a, b) > 0 && strcmp(a, c) > 0){
+        swap(a, c);
+    }
+    if(strcmp(b, a) > 0 && strcmp(b, c) > 0){
+        swap(b, c);
+    }
+    if(strcmp(a, b) > 0)
+    {
+        swap(a, b);
+    }
     
+    printf("%s, %s, %s\n", a,b,c);
     
-   
     return 0;
 }
